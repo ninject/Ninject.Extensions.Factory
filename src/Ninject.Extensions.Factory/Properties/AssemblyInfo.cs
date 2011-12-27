f76b9582ac5908befb83044eb6c26e1ba50cd93d
Remo Gloor <remo.gloor@bbv.ch>
2011-12-27 21:14:35 +0100

    Added assembly description for nuget

--- src/Ninject.Extensions.Factory/Properties/AssemblyInfo.cs
+++ src/Ninject.Extensions.Factory/Properties/AssemblyInfo.cs
@@ -25,6 +25,7 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ninject.Extensions.Factory")]
+[assembly: AssemblyDescriptionAttribute("Ninject extension that allows to automatically create factories.")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("39527a43-4711-438a-b3a8-63dc45fe45ee")]
